9e4e838d9cdb3ae368e82ec07f09d115d3b72529
Love AVL <51767480+Spartak-Belov-Floresku@users.noreply.github.com>
2021-05-30 00:16:12 -0400

    Update seed.py

--- seed.py
+++ seed.py
@@ -8,7 +8,7 @@ db.create_all()
 # If table isn't empty, empty it
 User.query.delete()
 
-# Add pets
+# Add users
 first_user = User(first_name="Tom", last_name="Smith", image_url="https://i.pinimg.com/originals/92/a6/2f/92a62f0221f58fe503a15fcb13f5c107.png")
 second_user = User(first_name="Jack", last_name="Sparrow", image_url="https://iadsb.tmgrup.com.tr/0999d9/0/0/0/0/2048/1199?u=https://idsb.tmgrup.com.tr/2017/03/19/jack-sparrow-might-be-inspired-by-a-muslim-captain-1489951367309.jpg")
 
