6962f7137920d4e0872ac1cef0e7e5c88fa6f3ea
Love AVL <51767480+Spartak-Belov-Floresku@users.noreply.github.com>
2021-05-31 16:44:38 -0400

    Update models.py

--- models.py
+++ models.py
@@ -11,7 +11,7 @@ def connect_db(app):
 
 class User(db.Model):
 
-    __tablename__ = "user"
+    __tablename__ = "users"
     
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     first_name = db.Column(db.String(25), nullable=False)
